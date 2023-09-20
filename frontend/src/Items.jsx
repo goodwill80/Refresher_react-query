64196db8102b52f76f5cb78b830e8e8220496d39
Ho Kit Jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2023-09-20 22:27:31 +0800

    post request with useMutation

--- frontend/src/Items.jsx
+++ frontend/src/Items.jsx
@@ -1,8 +1,29 @@
+import { useQuery } from '@tanstack/react-query';
 import SingleItem from './SingleItem';
-const Items = ({ items }) => {
+import customFetch from './util';
+
+const Items = () => {
+  // Query
+  const { isLoading, data, error, isError } = useQuery({
+    queryKey: ['tasks'],
+    queryFn: () => customFetch.get('/'),
+  });
+
+  if (isLoading) {
+    return <p style={{ marginTop: '1rem' }}>Loading...</p>;
+  }
+
+  if (isError) {
+    return <p style={{ marginTop: '1rem' }}>There was an error...</p>;
+  }
+  // Alternative
+  // if (error) {
+  //   return <p style={{ marginTop: '1rem' }}>{error.response.data}</p>;
+  // }
+
   return (
-    <div className='items'>
-      {items.map((item) => {
+    <div className="items">
+      {data.data.taskList.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
     </div>
